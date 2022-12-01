372240cdeb01e111ab2095ed8670b1af76569920
rivten <rivten@riseup.net>
2022-12-01 09:05:03 +0100

    rivten - day 01 - asm - part 2

--- 01/rivten/asm/day01.asm
+++ 01/rivten/asm/day01.asm
@@ -3,7 +3,8 @@ buffer: resb 0x100
 .len equ $ - buffer
 
 section .rodata
-fmt: db "result: %i", 10, 0
+fmt1: db "result part 1: %i", 10, 0
+fmt2: db "result part 2: %i", 10, 0
 
 section .text
 extern fgets
@@ -14,12 +15,42 @@ extern atoi
 
 global main
 
+adjust_elf:
+	cmp r12, rbx
+	jl .compare_second
+	; found heavier elf
+	mov r14, r13 ; the second becomes the third
+	mov r13, rbx ; the first becomes second
+	mov rbx, r12 ; the new one becomes the first
+	jmp .reset_elf
+
+.compare_second:
+	cmp r12, r13
+	jl .compare_third
+	; found heavier elf
+	mov r14, r13
+	mov r13, r12
+	jmp .reset_elf
+
+.compare_third:
+	cmp r12, r14
+	jl .reset_elf
+	; found heavier elf
+	mov r14, r12
+
+.reset_elf:
+	mov r12, 0
+	ret
+
 main:
 	push rbp
 	mov rbp, rsp
 
 	mov rbx, 0 ; rbx contains the result: the maximum found
-	mov r12, 0  ; r12 contains the sum of the weights for the current elf
+	mov r12, 0 ; r12 contains the sum of the weights for the current elf
+
+	mov r13, 0 ; r13 contains the weight of the 2nd heavier elf
+	mov r14, 0 ; r14 contains the weight of the 3rd heavier elf
 
 .start_loop:
 	lea rdi, [rel buffer]
@@ -42,25 +73,21 @@ main:
 	jmp .start_loop
 
 .end_elf:
-	cmp r12, rbx
-	jl .reset_elf
-	; found heavier elf
-	mov rbx, r12
-
-.reset_elf:
-	mov r12, 0
+	call adjust_elf
 	jmp .start_loop
 
 .end_loop:
-	; we still need to compare values for the last elf
-	cmp r12, rbx
-	jl .end
-	mov rbx, r12
+	call adjust_elf
 
-.end:
 	; display the result on the screen
-	lea rdi, [rel fmt]
+	lea rdi, [rel fmt1]
 	mov rsi, rbx
+	call printf wrt ..plt
+
+	lea rdi, [rel fmt2]
+	mov rsi, rbx
+	add rsi, r13
+	add rsi, r14
 	call printf wrt ..plt
 
 	mov rax, 0
