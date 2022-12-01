1bab5acb6c8b89bea4a6f74914ed648fcb4750f1
flavien <flavien@gensdeconfiance.com>
2022-12-01 22:29:20 +0100

    day 01

---
+++ 01/flaviengdc/index-1.mjs
@@ -0,0 +1,13 @@
+import { readFile } from "fs/promises";
+console.log(
+  (await readFile("./input.txt", { encoding: "utf8" }))
+    .split("\n")
+    .reduce(
+      (acc, current) =>
+        current === ""
+          ? [...acc, 0]
+          : [...acc.slice(0, -1), acc.at(-1) + current * 1],
+      [0]
+    )
+    .reduce((acc, current) => (current > acc ? current : acc), 0)
+);
---
+++ 01/flaviengdc/index-2.mjs
@@ -0,0 +1,14 @@
+import { readFile } from "fs/promises";
+console.log(
+  (await readFile("./input.txt", { encoding: "utf8" }))
+    .split("\n")
+    .reduce(
+      (acc, current) =>
+        current === ""
+          ? [...acc, 0]
+          : [...acc.slice(0, -1), acc.at(-1) + current * 1],
+      [0]
+    )
+    .sort((a, b) => b - a)
+    .reduce((acc, current, i) => (i < 3 ? acc + current : acc), 0)
+);
