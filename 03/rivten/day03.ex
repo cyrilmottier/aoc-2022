1ef8f54e76aa5fbdbc820b54ef39732a02dfddc1
rivten <rivten@riseup.net>
2022-12-03 11:09:01 +0100

    rivten - day 03 - elixir - part 1

---
+++ 03/rivten/day03.ex
@@ -0,0 +1,35 @@
+
+defmodule Day03 do
+  defp get_double_items_overall_and_length([], _, doubles, len) do
+    {doubles, len}
+  end
+
+  defp get_double_items_overall_and_length([head | tail], singles, doubles, len) do
+    case singles[head] do
+      nil -> get_double_items_overall_and_length(tail, Map.put(singles, head, len), doubles, len + 1)
+      pos -> get_double_items_overall_and_length(tail, singles, [{head, pos, len} | doubles], len + 1)
+    end
+  end
+
+  defp find_double_item(items) do
+    {doubles, len} = get_double_items_overall_and_length(String.graphemes(items), %{}, [], 0)
+    item = Enum.filter(doubles, fn({_, first_pos, other_pos}) -> first_pos * 2 < len && other_pos * 2 >= len end)
+           |> Enum.map(fn({item, _, _}) -> item end)
+           |> hd
+    <<item_value::utf8>> = item
+    cond do 
+      item_value >= 97 && item_value < 123 -> item_value - 96
+      item_value >= 65 && item_value < 91 -> item_value - 64 + 26
+      true -> raise "error"
+    end
+  end
+
+  def result() do
+    IO.read(:stdio, :eof)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&find_double_item/1)
+    |> Enum.sum
+    |> IO.inspect
+  end
+end
+
