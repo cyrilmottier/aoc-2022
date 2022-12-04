176b4b3e157579acd44ed0907abb7aabd3db24ea
flavien <flavien@gensdeconfiance.com>
2022-12-04 18:40:20 +0100

    day04

---
+++ 04/flaviengdc/index-1.mjs
@@ -0,0 +1,20 @@
+import { readFile } from "fs/promises";
+
+console.log(
+  (await readFile("./input.txt", { encoding: "utf8" }))
+    .split("\n")
+    .map((pair) => {
+      const [firstElf, secondElf] = pair.split(",");
+      const [firstElfStart, firstElfEnd] = firstElf.split("-");
+      const [secondElfStart, secondElfEnd] = secondElf.split("-");
+      return [
+        [Number(firstElfStart), Number(firstElfEnd)],
+        [Number(secondElfStart), Number(secondElfEnd)],
+      ];
+    })
+    .filter(
+      ([firstElf, secondElf]) =>
+        (firstElf[0] <= secondElf[0] && firstElf[1] >= secondElf[1]) ||
+        (secondElf[0] <= firstElf[0] && secondElf[1] >= firstElf[1])
+    ).length
+);
