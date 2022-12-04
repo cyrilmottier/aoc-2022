1c347f514d3752e1093fa3dc249ebcac092225b0
Cyril Mottier <cyril@gensdeconfiance.com>
2022-12-04 09:28:14 +0100

    day04

---
+++ 04/cyrilmottier/day.kts
@@ -0,0 +1,10 @@
+import java.io.File
+
+val SECTION_REG_EX = "(\\d+)-(\\d+),(\\d+)-(\\d+)".toRegex()
+
+File("input.txt").readLines()
+    .map { SECTION_REG_EX.find(it)!!.destructured.toList().map { it.toInt() } }
+    .count {
+        (it[0] >= it[2] && it[1] <= it[3]) ||
+                (it[2] >= it[0] && it[3] <= it[1])
+    }
