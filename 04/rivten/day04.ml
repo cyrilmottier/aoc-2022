41182b73718e2449fc8d67a8a968daf3cc775565
rivten <rivten@riseup.net>
2022-12-04 11:10:55 +0100

    rivten - day 04 - ocaml - part 2

--- 04/rivten/day04.ml
+++ 04/rivten/day04.ml
@@ -1,4 +1,4 @@
-let day01 =
+let input_parsed =
   In_channel.with_open_bin "input.txt" In_channel.input_all
   |> String.trim |> String.split_on_char '\n'
   |> List.map (fun s ->
@@ -7,5 +7,13 @@ let day01 =
          |> List.map (List.map int_of_string)
          |> fun [ [ a; b ]; [ c; d ] ] ->
          if a <= c then ((a, b), (c, d)) else ((c, d), (a, b)))
+
+let day01 =
+  input_parsed
   |> List.filter (fun ((a, b), (c, d)) -> a == c || d <= b)
+  |> List.length
+
+let day02 =
+  input_parsed
+  |> List.filter (fun ((a, b), (c, d)) -> a == c || c <= b)
   |> List.length
