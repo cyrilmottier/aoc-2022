1b7635e7af380d324c6529a0b58b597be72061b7
rivten <rivten@riseup.net>
2022-12-06 08:35:27 +0100

    rivten - day 06 - clojure - part 2

--- 06/rivten/day06.clj
+++ 06/rivten/day06.clj
@@ -8,10 +8,22 @@
       (count)
       (= 4)))
 
+(defn fourteen-different? [l]
+  (->> l
+      (take 14)
+      (map second)
+      (set)
+      (count)
+      (= 14)))
+
 (loop [elems (map vector (range) input)]
   (if (four-different? elems)
-    (println (first elems))
+    (println (->> (first elems) (first) (+ 4)))
+    (recur (rest elems))))
+
+(loop [elems (map vector (range) input)]
+  (if (fourteen-different? elems)
+    (println (->> (first elems) (first) (+ 14)))
     (recur (rest elems))))
       
-    
   
