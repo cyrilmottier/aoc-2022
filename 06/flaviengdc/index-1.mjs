acc7d79143f0b5e2ef50e94d01efccdde096dbb8
flavien <flavien@gensdeconfiance.com>
2022-12-06 18:45:52 +0100

    day06

---
+++ 06/flaviengdc/index-1.mjs
@@ -0,0 +1,9 @@
+import { readFile } from "fs/promises"
+
+console.log(
+  (await readFile("./input.txt", { encoding: "utf8" }))
+    .split("")
+    .map((_char, index, arr) => arr.slice(index - 4, index))
+    .map(marker => Array.from(new Set(marker)))
+    .findIndex(marker => marker.length === 4)
+)
