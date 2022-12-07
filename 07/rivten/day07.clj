a3ef6c66a4388a66cc27bffe28eb568bec3a7a33
rivten <rivten@riseup.net>
2022-12-07 14:20:32 +0100

    clean code

--- 07/rivten/day07.clj
+++ 07/rivten/day07.clj
@@ -1,7 +1,9 @@
 (defn get-directory-size
   [directory-sizes current-directory-size input]
   (cond 
-    (empty? input) [(conj directory-sizes current-directory-size) current-directory-size input]
+    (empty? input) 
+    [(conj directory-sizes current-directory-size) current-directory-size input]
+
     (re-find #"^\$ cd \.\." (first input)) 
     [(conj directory-sizes current-directory-size) 
      current-directory-size 
@@ -15,20 +17,27 @@
         (+ in-directory-size current-directory-size) 
         input))
 
-    (re-find #"^\$ ls" (first input))
-    (get-directory-size directory-sizes current-directory-size (rest input))
-
-    (re-find #"^dir" (first input)) 
-    (get-directory-size directory-sizes current-directory-size (rest input))
-
     (re-find #"^\d+ " (first input))
     (let [file-size (Integer/parseInt (first (clojure.string/split (first input) #" ")))]
       (get-directory-size directory-sizes (+ file-size current-directory-size) (rest input)))
 
     :else ((do (tap> input) (assert false)))))
-      
-(reduce + (filter #(< % 100000) (first (get-directory-size [] 0 (clojure.string/split (slurp "input.txt") #"\n")))))
 
-(let [sizes (first (get-directory-size [] 0 (clojure.string/split (slurp "input.txt") #"\n")))]
-  (let [space-to-free (- 30000000 (- 70000000 (apply max sizes)))]
-    (apply min (filter #(< space-to-free %) sizes))))
+(def cleaned-input 
+  (as-> (slurp "input.txt") $
+    (clojure.string/split $ #"\n")
+    (remove (partial re-find #"^(dir|\$ ls)") $)))
+
+; part 1
+(->> cleaned-input
+  (get-directory-size [] 0)
+  (first)
+  (filter (partial > 100000))
+  (reduce +))
+
+; part 2
+(let [sizes (first (get-directory-size [] 0 cleaned-input)) 
+      space-to-free (- 30000000 (- 70000000 (last sizes)))]
+  (->> sizes 
+       (filter (partial < space-to-free)) 
+       (apply min)))
