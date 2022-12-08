d6d945f175fdc7d8167de6a6da0c6265ed5e0fcb
Cyril Mottier <cyril@gensdeconfiance.com>
2022-12-08 14:04:37 +0100

    factorize day02-part2

--- 08/cyrilmottier/day08-part2.kts
+++ 08/cyrilmottier/day08-part2.kts
@@ -6,50 +6,28 @@ val forest = File("input.txt").readLines()
 val forestWidth = forest[0].size
 val forestHeight = forest.size
 
+fun t(row: Int, col: Int, rowDir: Int, colDir: Int): Int {
+    var result = 0
+    for (i in 1 until forestWidth) {
+        val r = row - rowDir * i
+        val c = col - colDir * i
+        if (c < 0 || c >= forestWidth || r < 0 || r >= forestHeight) break
+        result++
+        if (forest[r][c] >= forest[row][col]) {
+            break
+        }
+    }
+    return result
+}
+
 var maxScene = Int.MIN_VALUE
 for (row in 0 until forestHeight) {
     for (col in 0 until forestWidth) {
-        val treeHeight = forest[row][col]
-        // Left
-        var left = 0
-        for (i in 1 until forestWidth) {
-            val c = col - i
-            if (c < 0) break
-            left++
-            if (forest[row][c] >= treeHeight) {
-                break
-            }
-        }
-        // Right
-        var right = 0
-        for (i in 1 until forestWidth) {
-            val c = col + i
-            if (c >= forestWidth) break
-            right++
-            if (forest[row][c] >= treeHeight) {
-                break
-            }
-        }
-        // Top
-        var top = 0
-        for (i in 1 until forestWidth) {
-            val r = row - i
-            if (r < 0) break
-            top++
-            if (forest[r][col] >= treeHeight) {
-                break
-            }
-        }
-        // Bottom
-        var bottom = 0
-        for (i in 1 until forestWidth) {
-            val r = row + i
-            if (r >= forestHeight) break
-            bottom++
-            if (forest[r][col] >= treeHeight) {
-                break
-            }
-        }
+        val left = t(row, col, 0, -1)
+        val right = t(row, col, 0, 1)
+        val top = t(row, col, -1, 0)
+        val bottom = t(row, col, 1, 0)
+
         val scene = left * top * right * bottom
         if (scene > maxScene) {
             maxScene = scene
