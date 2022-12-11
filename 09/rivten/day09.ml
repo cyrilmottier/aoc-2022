014a4e5bafb9a56a17f29b9dbfbf3e4b8878e151
rivten <rivten@riseup.net>
2022-12-11 12:32:53 +0100

    rivten - day 09 - ocaml - part 2

--- 09/rivten/day09.ml
+++ 09/rivten/day09.ml
@@ -21,7 +21,6 @@ let do_head_move dir (hx, hy) =
   | _ -> failwith "do_head_move error"
 
 let sign x = if x == 0 then 0 else if x < 0 then -1 else 1
-
 let offset d = sign d * min 1 (Int.abs d)
 
 let do_tail_move (hx, hy) (tx, ty) =
@@ -34,15 +33,29 @@ let do_single_move dir h t =
   let t = do_tail_move h t in
   (h, t)
 
+let do_single_move_2 dir h t =
+  let h = do_head_move dir h in
+  t.(0) <- do_tail_move h t.(0);
+  for i = 1 to 8 do
+    t.(i) <- do_tail_move t.(i - 1) t.(i)
+  done;
+  (h, Array.copy t)
+
 let rec do_move dir count h t =
   if count == 0 then [ (h, t) ]
   else
     let h2, t2 = do_single_move dir h t in
     (h, t) :: do_move dir (count - 1) h2 t2
 
+let rec do_move_2 dir count h t =
+  if count == 0 then [ (h, t) ]
+  else
+    let h2, t2 = do_single_move_2 dir h t in
+    (h, t) :: do_move_2 dir (count - 1) h2 t2
+
 let move_reduce (h, t, tposes) (dir, count) =
   let moves = do_move dir count h t in
-  let (h, t) = List.hd (List.rev moves) in
+  let h, t = List.hd (List.rev moves) in
   let tposes =
     List.fold_left
       (fun s e -> S.add e s)
@@ -51,6 +64,25 @@ let move_reduce (h, t, tposes) (dir, count) =
   in
   (h, t, tposes)
 
-let day01 = 
-    let _, _, tposes = List.fold_left move_reduce ((0, 0), (0, 0), S.empty) moves
-    in S.cardinal tposes
+let move_reduce_2 (h, t, tposes) (dir, count) =
+  let moves = do_move_2 dir count h t in
+  let h, t = List.hd (List.rev moves) in
+  let tposes =
+    List.fold_left
+      (fun s e -> S.add e s)
+      tposes
+      (List.map (fun (_, tail) -> tail.(8)) moves)
+  in
+  (h, t, tposes)
+
+let day01 =
+  let _, _, tposes =
+    List.fold_left move_reduce ((0, 0), (0, 0), S.empty) moves
+  in
+  S.cardinal tposes
+
+let day02 =
+  let _, _, tposes =
+    List.fold_left move_reduce_2 ((0, 0), Array.make 9 (0, 0), S.empty) moves
+  in
+  S.cardinal tposes
