38aefdce623abfc190396646b2a6e3cd0d0a0e5c
rivten <rivten@riseup.net>
2022-12-15 18:27:25 +0100

    rivten - day 15 - nim - part 2

--- 15/rivten/day15.nim
+++ 15/rivten/day15.nim
@@ -20,11 +20,11 @@ let input = readFile("input.txt").splitLines()[0..^2]
 let sensorsBeacon = input.map(parseInput)
 let sensorsDistance = sensorsBeacon.map((t) => (t[0], manahattanDistance(t[0], t[1])))
 let knownBeacons = sensorsBeacon.map((t) => t[1]).toHashSet()
-echo knownBeacons
 
 let m = min(sensorsDistance.map((s) => s[0].x - s[1]))
 let M = max(sensorsDistance.map((s) => s[0].x + s[1]))
 
+# part 1
 var count = 0
 for x in countup(m, M):
   let b = (x: x, y: 2000000)
@@ -32,3 +32,24 @@ for x in countup(m, M):
     count += 1
 
 echo count
+
+# part 2
+iterator getAllPositionsAtDistFrom(s: P, d: int): P =
+  for i in countup(0, d):
+    yield (x: s.x + i, y: s.y + (d - i))
+    yield (x: s.x - i, y: s.y - (d - i))
+    yield (x: s.x + i, y: s.y - (d - i))
+    yield (x: s.x - i, y: s.y + (d - i))
+
+let MAX_BOUND = 4000000
+proc inBounds(p: P): bool =
+  p.x >= 0 and p.x < MAX_BOUND and p.y >= 0 and p.y < MAX_BOUND
+
+for s in sensorsDistance:
+  let sensor = s[0]
+  let d = s[1]
+  for p in getAllPositionsAtDistFrom(sensor, d + 1):
+    if inBounds(p) and canBeaconBeHere(p, sensorsDistance):
+      echo p.x * MAX_BOUND + p.y
+      quit()
+
