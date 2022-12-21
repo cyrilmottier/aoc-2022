ea9eda09b8adbaac25b5fcb7cac69aea2a0f6e90
Cyril Mottier <cyril@gensdeconfiance.com>
2022-12-21 07:24:12 +0100

    day21 - part1

---
+++ 21/cyrilmottier/day21.kts
@@ -0,0 +1,45 @@
+import java.io.File
+import kotlin.time.measureTime
+
+sealed class Expression
+data class Constant(val value: Long): Expression()
+data class Operation(val operation: Char, val left: String, val right: String): Expression()
+
+val expressions = File("input.txt").readLines()
+    .fold(mutableMapOf<String, Expression>()) { acc, line ->
+        val (variableName, expression) = line.split(": ")
+        val constant = expression.toLongOrNull()
+        acc[variableName] = if (constant != null) {
+             Constant(constant)
+        } else {
+            val left = expression.slice(0..3)
+            val right = expression.slice(7..10)
+            val operation = expression[5]
+            Operation(operation, left, right)
+        }
+        acc
+    }
+
+fun compute(varName: String): Long {
+    val result = expressions[varName]
+    return when (result) {
+        is Constant -> result.value
+        is Operation -> {
+            val operation: Long.(Long) -> Long = when (result.operation) {
+                '+' -> Long::plus
+                '-' -> Long::minus
+                '*' -> Long::times
+                '/' -> Long::div
+                else -> throw UnsupportedOperationException()
+            }
+            compute(result.left).operation(compute(result.right))
+        }
+        else -> throw IllegalStateException()
+    }
+}
+
+@OptIn(kotlin.time.ExperimentalTime::class)
+val duration = measureTime {
+    println(compute("root"))
+}
+println("Took $duration")
